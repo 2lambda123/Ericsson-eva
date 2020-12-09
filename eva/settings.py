f6c13f06971d562a0e477423a446c7ce6c08d3ea
Jason Hilton <jhilton@toyon.corp>
2020-12-09 09:03:29 -0800

    remove limit on DATA_UPLOAD_MAX_MEMORY_SIZE
    eva uses posts requests to update tracks with new vehicles.
    By default if a post request is greater than 2.5mb, django
    will truncate that post requst. Setting this paramater to None
    disables the check that does this. Which fixes a bug where labeling
    stops after a large set of images.
    
    see https://docs.djangoproject.com/en/1.11/ref/settings/#data-upload-max-memory-size

--- eva/settings.py
+++ eva/settings.py
@@ -21,6 +21,7 @@ VIDEO_CHUNK_SIZE = 100
 # This setting controls how small the last chunk can be. If it is smaller,
 # the frames are added to the previous chunk instead.
 MIN_FINAL_CHUNK_SIZE = VIDEO_CHUNK_SIZE // 2
+DATA_UPLOAD_MAX_MEMORY_SIZE = None
 
 # Defines extra bounding box scaling before tracking
 TRACKER_SCALING = 2
